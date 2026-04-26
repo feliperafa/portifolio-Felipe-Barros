2dd25553dd7c85d7cc4e74d5bf03cfa98121c43a
Felipe Barros <feliperafael_barros@hotmail.com>
2026-04-26 01:39:01 -0300

    Ajustando caminhos

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const basePath = repoName ? `/${repoName}` : '';
+
 const nextConfig = {
-	output: 'export',
+  output: 'export',
+  basePath,
+  assetPrefix: basePath || './',
+  trailingSlash: true,
 };
+
 export default nextConfig;
